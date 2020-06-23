f0ca8bd9be3d4b5969594193ae7b39501f717024
nmv13 <vel16002@byui.edu>
2020-06-22 20:09:09 -0600

    initial push

---
+++ riskdice.cpp
@@ -0,0 +1,464 @@
+/****************************************************************************
+* RiskDice: This program allows the user to select from two different modes.
+* The first mode is for a single dice roll, allowing the attacker to go from
+* one attack at a time. The second is a blitz so the attacker doesn't need
+* to roll for a while before coming to a concluding result.
+****************************************************************************/
+
+#include <iomanip>
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctime>
+
+using namespace std;
+
+int numDie(int x, char c);
+void faceOff(int dieA, int dieD, int * numA, int * numD, int mode);
+void a1d1(int * numA, int * numD, int mode);
+void a2d1(int * numA, int * numD, int mode);
+void a1d2(int * numA, int * numD, int mode);
+void a2d2(int * numA, int * numD, int mode);
+void a3d1(int * numA, int * numD, int mode);
+void a3d2(int * numA, int * numD, int mode);
+void result(int numA, int numD, int th);
+
+int main()
+{
+	int mode; // Whether the user wanrs Normal or Blitz
+	int numA; // Number of troops Attacker has
+	int numD; // Number of troops Defender has
+	int dieA; // Number of dice Attacker has
+	int dieD; // Number of dice Defender has
+	int th;   // Threshhold variable
+	char ans; // The user answer if it's single mode
+
+	// Prompt user, which mode they want
+	cout << "What mode to you want?\n\n   1: Normal\n   2: Blitz\n\nSelect: ";
+	cin  >> mode;
+
+	// Prompt the user for number of troops attacking
+	cout << "Number of troops Attacking: ";
+	cin  >> numA;
+
+	// Prompt the user for number of troops defending
+	cout << "Number of troops Defending: ";
+	cin  >> numD;
+
+	// Prompt the Attacker for desired threshhold if count is high and close
+	if (numA > 50 && ((numA - numD) <= 20))
+	{
+		cout << "Attacking threshhold (" << (numA - 50) << " troops or less): ";
+		cin  >> th;
+	}
+	else
+		th = 1;
+
+	// Buffer new line
+	cout << endl;
+
+	// Single die roll mode
+	if (mode == 1)
+	{
+		for(;;)
+		{
+			system("clear");
+			// Defener has a max of 2 die for 2+ people
+			dieD = numDie(numD, 'D');
+			
+			// Attacker has a max of 3 for 3+ people
+			dieA = numDie(numA, 'A');
+
+			// Battle
+			faceOff(dieA, dieD, &numA, &numD, mode);
+
+			// Keep going?
+			if (numD <= 0 || numA <= 1)
+			{
+				result(numA, numD, th);
+				return 0;
+			}
+			else
+			{
+				cout << "\nEnter 'a' to continue attacking, 'c' to cease fire: ";
+				cin  >> ans;
+			}
+
+			// Stop if attacker decides to stop
+			if (ans == 'c' || ans == 'C')
+			{
+				result(numA, numD, th);
+				return 0;
+			}
+		}
+	}
+
+	// Initiate rolls : https://www.bitdegree.org/learn/random-number-generator-cpp
+  	srand((unsigned) time(0));
+  	do
+  	{		
+		// Attacker has a max of 3 for 3+ people
+		dieA = numDie(numA, 'A');
+
+		// Defener has a max of 2 die for 2+ people
+		dieD = numDie(numD, 'D');
+
+		// Battle
+		faceOff(dieA, dieD, &numA, &numD, mode);
+  		
+  	} while (numD > 0 && numA > th);
+
+  	result(numA, numD, th);
+
+  	return 0;
+}
+
+/**********************************************
+* Find how many die each side is able to use
+**********************************************/
+int numDie(int x, char c)
+{
+	int die;
+
+	// Defener has a max of 2 die for 2+ people
+	if (c == 'D')
+	{
+		if (x == 1)
+			die = 1;
+		else if (x >= 2)
+			die = 2;
+	}
+	// Attacker has a max of 3 for 3+ people
+	else if (c == 'A')
+	{
+		if (x == 2)
+			die = 1;
+		else if (x == 3)
+			die = 2;
+		else if (x > 3)
+			die = 3;
+	}
+
+	return die;
+}
+
+/**********************************************
+* Determine battle function based on die count
+**********************************************/
+void faceOff(int dieA, int dieD, int * numA, int * numD, int mode)
+{
+	if (dieA == 1 && dieD == 1)
+	{
+		a1d1(numA, numD, mode);
+	}
+	else if (dieA == 2 && dieD == 1)
+	{
+		a2d1(numA, numD, mode);
+	}
+	else if (dieA == 1 && dieD == 2)
+	{
+		a1d2(numA, numD, mode);
+	}
+	else if (dieA == 3 && dieD == 1)
+	{
+		a3d1(numA, numD, mode);
+	}
+	else if (dieA == 2 && dieD == 2)
+	{
+		a2d2(numA, numD, mode);
+	}
+	else if (dieA == 3 && dieD == 2)
+	{
+		a3d2(numA, numD, mode);	
+	}
+}
+
+/**********************************************
+* Attacker has 1 die, defender has 1
+**********************************************/
+void a1d1(int * numA, int * numD, int mode)
+{
+	int rollD1 = 1 + (rand() % 6);
+	int rollA1 = 1 + (rand() % 6);
+
+	if (mode == 1)
+		cout << "Attacker = " << rollA1 << "\nDefender = " << rollD1 << endl;
+
+	if (rollD1 >= rollA1)
+	{
+		*numA = *numA - 1;
+		if (mode == 1)
+			cout << "\nAttacker - 1\n";
+	}
+	else
+	{
+		*numD = *numD - 1;
+		if (mode == 1)
+			cout << "\nDefender - 1\n";
+	}
+
+	if (mode == 1)
+		cout << "\n---\nnumA = " << *numA << "\nnumD = " << *numD << "\n---\n";
+}
+
+/**********************************************
+* Attacker has 2 die, defender has 1
+**********************************************/
+void a2d1(int * numA, int * numD, int mode)
+{
+	int rollD1 = 1 + (rand() % 6);
+	int rollA1 = 1 + (rand() % 6);
+	int rollA2 = 1 + (rand() % 6);
+
+	if (mode == 1)
+		cout << "Attacker = " << rollA1 << ", " << rollA2 << "\nDefender = " << rollD1 << endl;
+
+	if (rollD1 >= rollA1 || rollD1 >= rollA2)
+	{
+		*numA = *numA - 1;
+		if (mode == 1)
+			cout << "\nAttacker - 1\n";
+	}
+	else
+	{
+		*numD = *numD - 1;
+		if (mode == 1)
+			cout << "\nDefender - 1\n";
+	}
+
+	if (mode == 1)
+		cout << "\n---\nnumA = " << *numA << "\nnumD = " << *numD << "\n---\n";
+}
+
+/**********************************************
+* Attacker has 1 die, defender has 2
+**********************************************/
+void a1d2(int * numA, int * numD, int mode)
+{
+	int rollD1 = 1 + (rand() % 6);
+	int rollD2 = 1 + (rand() % 6);
+	int rollA1 = 1 + (rand() % 6);
+
+	if (mode == 1)
+		cout << "Attacker = " << rollA1 << "\nDefender = " << rollD1 << ", " << rollD2 << endl;
+
+
+	if (rollA1 < rollD1 && rollA1 < rollD2)
+	{
+		*numA = *numA - 1;
+		if (mode == 1)
+			cout << "\nAttacker - 1\n";
+	}
+	else
+	{
+		*numD = *numD - 1;
+		if (mode == 1)
+			cout << "\nDefender - 1\n";
+	}
+
+	if (mode == 1)
+		cout << "\n---\nnumA = " << *numA << "\nnumD = " << *numD << "\n---\n";
+}
+
+/**********************************************
+* Attacker has 3 die, defender has 1
+**********************************************/
+void a3d1(int * numA, int * numD, int mode)
+{
+	int rollD1 = 1 + (rand() % 6);
+	int rollA1 = 1 + (rand() % 6);
+	int rollA2 = 1 + (rand() % 6);
+	int rollA3 = 1 + (rand() % 6);
+
+	if (mode == 1)
+		cout << "Attacker = " << rollA1 << ", " << rollA2 << ", " << rollA3 << "\nDefender = " << rollD1 << endl;
+
+	if (rollD1 >= rollA1 && rollD1 >= rollA2 && rollD1 >= rollA3)
+	{
+		*numA = *numA - 1;
+		if (mode == 1)
+			cout << "\nAttacker - 1\n";
+	}
+	else
+	{
+		*numD = *numD - 1;
+		if (mode == 1)
+			cout << "\nDefender - 1\n";
+	}
+
+	if (mode == 1)
+		cout << "\n---\nnumA = " << *numA << "\nnumD = " << *numD << "\n---\n";
+}
+
+/**********************************************
+* Attacker has 2 die, defender has 2
+**********************************************/
+void a2d2(int * numA, int * numD, int mode)
+{
+	int rollD1 = 1 + (rand() % 6);
+	int rollD2 = 1 + (rand() % 6);
+	int rollA1 = 1 + (rand() % 6);
+	int rollA2 = 1 + (rand() % 6);
+	int largeD, smallD, largeA, smallA;
+
+	if (mode == 1)
+		cout << "Attacker = " << rollA1 << ", " << rollA2 << "\nDefender = " << rollD1 << ", " << rollD2 << endl;
+
+
+	// Find Defender die values
+	if (rollD1 > rollD2)
+	{
+		largeD = rollD1;
+		smallD = rollD2;
+	}
+	else
+	{
+		largeD = rollD2;
+		smallD = rollD1;
+	}
+
+	// Find attacker die values
+	if (rollA1 > rollA2)
+	{
+		largeA = rollA1;
+		smallA = rollA2;
+	}
+	else
+	{
+		largeA = rollA2;
+		smallA = rollA1;
+	}
+
+
+	if (largeD >= largeA && smallD >= smallA)
+	{
+		*numA = *numA - 2;
+		if (mode == 1)
+			cout << "\nAttacker - 2\n";
+	}
+	else if (largeD >= largeA || smallD >= smallA)
+	{
+		*numA = *numA - 1;
+		*numD = *numD - 1;
+		if (mode == 1)
+			cout << "\nAttacker - 1\nDefender - 1\n";
+	}
+	else
+	{
+		*numD = *numD - 2;
+		if (mode == 1)
+		cout << "\nDefender - 2\n";
+	}
+
+	if (mode == 1)
+		cout << "\n---\nnumA = " << *numA << "\nnumD = " << *numD << "\n---\n";
+}
+
+/**********************************************
+* Attacker has 3 die, defender has 2
+**********************************************/
+void a3d2(int * numA, int * numD, int mode)
+{
+	int rollD1 = 1 + (rand() % 6);
+	int rollD2 = 1 + (rand() % 6);
+	int rollA1 = 1 + (rand() % 6);
+	int rollA2 = 1 + (rand() % 6);
+	int rollA3 = 1 + (rand() % 6);
+	int largeD, smallD, largeA, smallA;
+
+	if (mode == 1)
+		cout << "Attacker = " << rollA1 << ", " << rollA2 << ", " << rollA3 << "\nDefender = " << rollD1 << ", " << rollD2 << endl;
+
+	// Find Defender die values
+	if (rollD1 > rollD2)
+	{
+		largeD = rollD1;
+		smallD = rollD2;
+	}
+	else
+	{
+		largeD = rollD2;
+		smallD = rollD1;
+	}
+
+	// Find attacker die values
+	if (rollA1 > rollA2 && rollA1 > rollA3)
+	{
+		largeA = rollA1;
+		if (rollA2 >= rollA3)
+		{
+			smallA = rollA2;
+		}
+		else
+		{
+			smallA = rollA3;
+		}
+	}
+	else if (rollA2 > rollA1 && rollA2 > rollA3)
+	{
+		largeA = rollA2;
+		if (rollA1 >= rollA3)
+		{
+			smallA = rollA1;
+		}
+		else
+		{
+			smallA = rollA3;
+		}
+	}
+	else
+	{
+		largeA = rollA3;
+		if (rollA1 >= rollA2)
+		{
+			smallA = rollA1;
+		}
+		else
+		{
+			smallA = rollA2;
+		}
+	}
+
+
+	if (largeD >= largeA && smallD >= smallA)
+	{
+		*numA = *numA - 2;
+		if (mode == 1)
+			cout << "\nAttacker - 2\n";
+	}
+	else if (largeD >= largeA || smallD >= smallA)
+	{
+		*numA = *numA - 1;
+		*numD = *numD - 1;
+		if (mode == 1)
+			cout << "\nAttacker - 1\nDefender - 1\n";
+	}
+	else
+	{
+		*numD = *numD - 2;
+		if (mode == 1)
+		cout << "\nDefender - 2\n";
+	}
+
+	if (mode == 1)
+		cout << "\n---\nnumA = " << *numA << "\nnumD = " << *numD << "\n---\n";
+}
+
+/**********************************************
+* Display the results of the battle
+**********************************************/
+void result(int numA, int numD, int th)
+{
+  	// Adjust
+  	if (numD < 0)
+  		numD = 0;
+  	if (numA < 0)
+  		numA = 0;
+
+  	if (numD == 0)
+  			cout << "\nAttacker Won!\nAttacker has " << numA << " troop(s) remaining\nDefender has " << numD << " troop(s) remaining\n";
+  	else if (numA == 1)
+  			cout << "\nAttacker Lost!\nAttacker has " << numA << " troop(s) remaining\nDefender has " << numD << " troop(s) remaining\n";
+  	else
+  			cout << "\nAttacker Cease Fire!\nAttacker has " << numA << " troop(s) remaining\nDefender has " << numD << " troop(s) remaining\n";
+}
---
+++ ttt.cpp
@@ -0,0 +1,484 @@
+#include <iomanip>
+#include <iostream>
+#include <time.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <vector>
+
+using namespace std;
+
+/**************************************
+* Current Bug: on the computer goes first mode, game is ending pre-maturely
+**************************************/
+
+void game(int mode, int key[9], char board[9], int difficulty, int turn);
+char find(int a, int b, int c, vector<int> p, char board[9], char xo);
+char gameVScpu(char board[9], int turnF, int key[9], int difficulty);
+bool check(char board[9], char p);
+template <class T> void display(T arr);
+
+bool GO = true;
+
+int main()
+{
+	int mode;
+	int difficulty;
+	int turn;
+	bool ans;
+	
+	do
+	{
+		// create array for key
+		int key[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+		// Create array for playing board
+		char board[9] = {32, 32, 32, 32, 32, 32, 32, 32, 32};
+
+
+		cout << "Welcome!\n\n   1: 2-Person Player\n   2: Computer Player\n\nPlease choose mode: ";
+		cin  >> mode;
+
+		if (mode == 2)
+		{
+			cout << "Please choose a difficulty:\n\n   1: Easy\n   2: Standard\n   3: Impossible\n\nDifficulty: ";
+			cin  >> difficulty; 
+
+			cout << "Choose your turn:\n\n   1: First\n   2: Second\n\nPreference: ";
+			cin  >> turn;
+		}
+
+		display(key);
+		display(board);
+		game(mode, key, board, difficulty, turn);
+
+		cout << "Enter 0 to quit, 1 to keep playing: ";
+		cin  >> ans;
+
+	} while (ans);
+
+	cout << "Thanks for playing!\n";
+
+	return 0;
+}
+
+char find(int a, int b, int c, vector<int> p, char board[9], char xo)
+{
+	// sleep(1);
+	for (int i = 0; i < p.size(); i++)
+	{
+		if (p[i] == a)
+		{
+			for (int j = (i + 1); j < p.size(); j++)
+			{
+				if (p[j] == b && board[c] != xo)
+				{
+					board[c] = 'O';
+					GO = false;
+					return board[9];
+				}
+				else if (p[j] == c && board[b] != xo)
+				{
+					board[b] = 'O';
+					GO = false;
+					return board[9];
+				}
+			}
+		}
+		else if (p[i] == b)
+		{
+			for (int j = (i + 1); j < p.size(); j++)
+			{
+				if (p[j] == c && board[a] != xo)
+				{
+					board[a] = 'O';
+					GO = false;
+					return board[9];
+				}
+			}
+		}
+	}
+	return board[9];
+}
+
+char gameVScpu(char board[9], int turnF, int key[9], int difficulty)
+{
+	int n = 0;
+
+	if (difficulty == 1)
+	{
+		while (n < 10)
+		{
+			// sleep(1);
+			srand (time(NULL));
+			n = rand() % 8;
+			if (board[n] == ' ')
+			{
+				board[n] = 'O';
+				n = 10;
+			}
+		}
+	}
+	else if (difficulty == 2)
+	{
+		vector<int> x; // X
+		vector<int> o; // O
+		vector<int> s; // Space
+
+		bool r1x = false; // {0,1,2}
+		bool r2x = false; // {3,4,5}
+		bool r3x = false; // {6,7,8}
+		bool c1x = false; // {0,3,6}
+		bool c2x = false; // {1,4,7}
+		bool c3x = false; // {2,5,8}
+		bool ddx = false; // {0,4,8}
+		bool dux = false; // {2,4,6}
+
+		bool r1o = false; // {0,1,2}
+		bool r2o = false; // {3,4,5}
+		bool r3o = false; // {6,7,8}
+		bool c1o = false; // {0,3,6}
+		bool c2o = false; // {1,4,7}
+		bool c3o = false; // {2,5,8}
+		bool ddo = false; // {0,4,8}
+		bool duo = false; // {2,4,6}
+		GO = true;
+
+		// find where 'X's, 'O's, and spaces are on the board
+		for (int i = 0; i < 9; i++)
+		{
+			if (board[i] == 'X')
+				x.push_back(i);
+			else if (board[i] == 'O')
+				o.push_back(i);
+			else
+				s.push_back(i);
+		}
+
+		// 'X's
+		for (int z = 0; z < x.size(); z++)
+		{
+			if (x[z] == 0)
+			{
+				r1x = true; c1x = true; ddx = true;
+			}
+			if (x[z] == 1)
+			{
+				r1x = true; c2x = true;
+			}
+			if (x[z] == 2)
+			{
+				r1x = true; c3x = true; dux = true;
+			}
+			if (x[z] == 3)
+			{
+				r2x = true; c1x = true;
+			}
+			if (x[z] == 4)
+			{
+				r2x = true; c2x = true; ddx = true; dux = true; 
+			}
+			if (x[z] == 5)
+			{
+				r2x = true; c3x = true;
+			}
+			if (x[z] == 6)
+			{
+				r3x = true; c1x = true; dux = true;
+			}
+			if (x[z] == 7)
+			{
+				r3x = true; c2x = true;
+			}
+			if (x[z] == 8)
+			{
+				r3x = true; c3x = true; ddx = true;
+			}
+		}
+
+		// 'O's
+		for (int z = 0; z < o.size(); z++)
+		{
+			if (o[z] == 0)
+			{
+				r1o = true; c1o = true; ddo = true;
+			}
+			if (o[z] == 1)
+			{
+				r1o = true; c2o = true;
+			}
+			if (o[z] == 2)
+			{
+				r1o = true; c3o = true; duo = true;
+			}
+			if (o[z] == 3)
+			{
+				r2o = true; c1o = true;
+			}
+			if (o[z] == 4)
+			{
+				r2o = true; c2o = true; ddo = true; duo = true; 
+			}
+			if (o[z] == 5)
+			{
+				r2o = true; c3o = true;
+			}
+			if (o[z] == 6)
+			{
+				r3o = true; c1o = true; duo = true;
+			}
+			if (o[z] == 7)
+			{
+				r3o = true; c2o = true;
+			}
+			if (o[z] == 8)
+			{
+				r3o = true; c3o = true; ddo = true;
+			}
+		}
+
+		// Check for 'O's
+		if (r1o)
+			board[9] = find(0, 1, 2, o, board, 'X');
+		if (r2o && GO)
+			board[9] = find(3, 4, 5, o, board, 'X');
+		if (r3o && GO)
+			board[9] = find(6, 7, 8, o, board, 'X');
+		if (c1o && GO)
+			board[9] = find(0, 3, 6, o, board, 'X');
+		if (c2o && GO)
+			board[9] = find(1, 4, 7, o, board, 'X');
+		if (c3o && GO)
+			board[9] = find(2, 5, 8, o, board, 'X');
+		if (ddo && GO)
+			board[9] = find(0, 4, 8, o, board, 'X');
+		if (duo && GO)
+			board[9] = find(2, 4, 6, o, board, 'X');
+
+		// Check for 'X's
+		if (r1x && GO)
+			board[9] = find(0, 1, 2, x, board, 'O');
+		if (r2x && GO)
+			board[9] = find(3, 4, 5, x, board, 'O');
+		if (r3x && GO)
+			board[9] = find(6, 7, 8, x, board, 'O');
+		if (c1x && GO)
+			board[9] = find(0, 3, 6, x, board, 'O');
+		if (c2x && GO)
+			board[9] = find(1, 4, 7, x, board, 'O');
+		if (c3x && GO)
+			board[9] = find(2, 5, 8, x, board, 'O');
+		if (ddx && GO)
+			board[9] = find(0, 4, 8, x, board, 'O');
+		if (dux && GO)
+			board[9] = find(2, 4, 6, x, board, 'O');
+		if (GO)
+		{
+			while (n < 10)
+			{
+				cout << ".";
+				sleep(1);
+				srand (time(NULL));
+				n = rand() % 8;
+				if (board[n] == ' ')
+				{
+					board[n] = 'O';
+					n = 10;
+				}
+			}
+		}
+	}
+	else
+	{
+		if (turnF == 2)
+		{
+			if (board[4] == 32)
+			{
+				board[4] = 'O';
+			}
+			else
+			{
+				board[6] = 'O';
+			}
+		}
+		else if (turnF == 3 && board[6] == 'O')
+		{
+			if (board[0] == 'X')
+				board[8] = 'O';
+			else if (board[8] == 'X')
+				board[0] = 'O';
+			else if (board[1] == 'X')
+				board[7] = 'O';
+			else if (board[7] == 'X')
+				board[1] = 'O';
+			else if (board[3] == 'X')
+				board[5] = 'O';
+			else if (board[5] == 'X')
+				board[3] = 'O';
+			else
+				board[8] = 'O';
+		}
+		else if (turnF == 3 && board[4] == 'O')
+			if (board[1] == 'X' || board[3] == 'X' || board[5] == 'X' || board[7] == 'X')
+				board[6] = 'O';
+			// else if (board[] == 'X')
+	}
+
+	display(key);
+	display(board);
+
+	return board[9];
+}
+
+void game(int mode, int key[9], char board[9], int difficulty, int turn)
+{
+	// Initialize move variables
+	int p1, p2;
+	bool end = false;
+	bool good = false;
+	bool go = true;
+	int turnF = 1;
+
+	do
+	{
+		if (turn == 1)
+		{
+			do
+			{
+				// Ask for move from player 1
+				cout << "Player X, make your move: ";
+				cin  >> p1;
+
+				// Check for move validity
+				if (p1 > 0 && p1 < 10 && board[p1 - 1] == 32)
+				{
+					board[p1 - 1] = 'X';
+					good = true;
+				}
+				else
+					cout << "error: The number you entered is invalid or the square is already filled.\n";
+			} while (!good);
+
+			if (turnF >= 3 && check(board, 'X'))
+			{
+				// system("clear");
+				cout << "Player X Wins!";
+				end = true;
+				go = false;
+				display(board);
+			}
+			else if (turnF >= 5)
+			{
+				// system("clear");
+				cout << "It's A Tie!\n";
+				end = true;
+				go = false;
+				display(board);
+			}
+			else
+			{
+				good = false;
+				display(key);
+				display(board);
+				turnF++;
+			}
+		}
+		else
+		{
+			turn = 1;
+			turnF++;
+		}
+
+		if (mode == 1)
+		{
+			while (!good)
+			{
+				// Ask for move from player 2
+				cout << "Player O, make your move: ";
+				cin  >> p2;
+
+				// Check for move validity
+				if (p2 > 0 && p2 < 10 && board[p2 - 1] == 32)
+				{
+					board[p2 - 1] = 'O';
+					good = true;
+				}
+				else
+						cout << "error: The number you entered is invalid or the square is already filled.\n";
+			}
+
+			if (turnF >= 3 && check(board, 'O') && go)
+			{
+				// system("clear");
+				cout << "Player O Wins!";
+				end = true;
+				display(board);
+			}
+			else if (go)
+			{
+				good = false;
+				display(key);
+				display(board);
+			}
+		}
+		else if (go)
+		{
+			board[9] = gameVScpu(board, turnF, key, difficulty);
+
+			if (turnF >= 3 && check(board, 'O') && go)
+			{
+				// system("clear");
+				cout << "Computer Wins!";
+				end = true;
+				display(board);
+			}
+		}
+		turnF++;
+	} while (!end);
+}
+
+bool check(char board[9], char p)
+{
+	// Check for all possible win positions for X
+	if (board[0] == p && board[1] == p && board[2] == p) 
+		return true;
+	else if (board[3] == p && board[4] == p && board[5] == p)
+		return true;
+	else if (board[6] == p && board[7] == p && board[8] == p)
+		return true;
+	else if (board[0] == p && board[3] == p && board[6] == p)
+		return true;
+	else if (board[1] == p && board[4] == p && board[7] == p)
+		return true;
+	else if (board[2] == p && board[5] == p && board[8] == p)
+		return true;
+	else if (board[0] == p && board[4] == p && board[8] == p)
+		return true;
+	else if (board[6] == p && board[4] == p && board[2] == p)
+		return true;
+	else
+		return false;
+}
+
+template <class T>
+void display(T arr)
+{
+	if (arr[0] == 1)
+	{
+		// system("clear");
+		cout << "\n\n\n                               KEY\n";
+	}
+	else
+		cout << "\n\n\n                               PLAY\n";
+
+	cout << "\n                     |                     |                  \n"
+	     << "          " << arr[0] << "          |          " << arr[1] << "          |          " << arr[2] << endl
+	     << "                     |                     |                  \n"
+	     << "------------------------------------------------------------------\n"
+	     << "                     |                     |                  \n"
+	     << "          " << arr[3] << "          |          " << arr[4] << "          |          " << arr[5] << endl
+	     << "                     |                     |                  \n"
+	     << "------------------------------------------------------------------\n"
+	     << "                     |                     |                  \n"
+	     << "          " << arr[6] << "          |          " << arr[7] << "          |          " << arr[8] << endl
+	     << "                     |                     |                  \n\n\n\n";
+}
